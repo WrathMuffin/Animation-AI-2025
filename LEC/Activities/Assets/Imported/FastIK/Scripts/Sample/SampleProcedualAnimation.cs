2a8c40fc46d819f593f1a6b5df1bd24029b1ca66
Chuc <65578837+WrathMuffin@users.noreply.github.com>
2025-11-02 17:12:54 -0500

    ACTUVUTYEYEYE 444444444444444
    very janky but it kinda works :(
    REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- LEC/Activities/Assets/Imported/FastIK/Scripts/Sample/SampleProcedualAnimation.cs
+++ LEC/Activities/Assets/Imported/FastIK/Scripts/Sample/SampleProcedualAnimation.cs
@@ -6,8 +6,8 @@ namespace DitzelGames.FastIK
     {
         public Transform[] FootTarget;
         public Transform LookTarget;
-        public Transform HandTarget;
-        public Transform HandPole;
+        public Transform[] HandTarget;
+        public Transform[] HandPole;
         public Transform Step;
         public Transform Attraction;
 
@@ -15,30 +15,40 @@ namespace DitzelGames.FastIK
         {
             //move step & attraction
             Step.Translate(Vector3.forward * Time.deltaTime * 0.7f);
+
             if (Step.position.z > 1f)
+
                 Step.position = Step.position + Vector3.forward * -2f;
             Attraction.Translate(Vector3.forward * Time.deltaTime * 0.5f);
+
             if (Attraction.position.z > 1f)
-                Attraction.position = Attraction.position + Vector3.forward * -2f;
+                Attraction.position = Attraction.position + Vector3.forward * -5f;
 
             //footsteps
             for(int i = 0; i < FootTarget.Length; i++)
             {
                 var foot = FootTarget[i];
                 var ray = new Ray(foot.transform.position + Vector3.up * 0.5f, Vector3.down);
+
                 var hitInfo = new RaycastHit();
+
                 if(Physics.SphereCast(ray, 0.05f, out hitInfo, 0.50f))
-                    foot.position = hitInfo.point + Vector3.up * 0.05f;
+                    foot.transform.position = hitInfo.point + Vector3.up * 0.05f;
             }
 
             //hand and look
             var normDist = Mathf.Clamp((Vector3.Distance(LookTarget.position, Attraction.position) - 0.3f) / 1f, 0, 1);
-            HandTarget.rotation = Quaternion.Lerp(Quaternion.Euler(90, 0, 0), HandTarget.rotation, normDist);
-            HandTarget.position = Vector3.Lerp(Attraction.position, HandTarget.position, normDist);
-            HandPole.position = Vector3.Lerp(HandTarget.position + Vector3.down * 2, HandTarget.position + Vector3.forward * 2f, normDist);
+
+            for (int i = 0; i < HandTarget.Length; i++)
+            {
+                var hand = HandTarget[i];
+                var pole = HandPole[i];
+                hand.rotation = Quaternion.Lerp(Quaternion.Euler(90, 0, 0), hand.rotation, normDist);
+                hand.position = Vector3.Lerp(Attraction.position, hand.position, normDist);
+                pole.position = Vector3.Lerp(hand.position + Vector3.down * 2, hand.position + Vector3.forward * 2f, normDist);
+            }
+
             LookTarget.position = Vector3.Lerp(Attraction.position, LookTarget.position, normDist);
-
-
         }
 
     }
---
+++ LEC/Activities/Assets/Imported/FastIK/Scripts/Sample/SampleRotator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace DitzelGames.FastIK
+{
+
+    public class SampleRotator : MonoBehaviour
+    {
+        public bool rotateOnX, rotateOnY = true, rotateOnZ;
+
+        public float speed = 180.0f;
+
+        void Update()
+        {
+            //just rotate the object, check the axis it should rotate from
+
+            if (rotateOnX)
+            {
+                transform.Rotate(Time.deltaTime * speed, 0, 0);
+            }
+
+            if (rotateOnY)
+            {
+                transform.Rotate(0, Time.deltaTime * speed, 0);
+            }
+
+            if (rotateOnZ)
+            {
+                transform.Rotate(0, 0, Time.deltaTime * speed);
+            }
+        }
+    }
+}
